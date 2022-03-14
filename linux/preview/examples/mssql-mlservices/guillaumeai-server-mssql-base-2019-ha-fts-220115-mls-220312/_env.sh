bcdddf9b32788f8a7582f489f35546c17b18d974
Jean Guillaume <jgi@jgwill.com>
2022-03-13 22:29:34 -0400

    chg: SQL Machine Learning setup for Gia dist

---
+++ linux/preview/examples/mssql-mlservices/guillaumeai-server-mssql-base-2019-ha-fts-220115-mls-220312/_env.sh
@@ -0,0 +1,5 @@
+export dockertag=guillaumeai/server:mssql-base-2019-ha-fts-220115-mls-220312
+export dockertag2=guillaumeai/server:mssql-mls-220312b
+#FROM guillaumeai/server:mssql-base-2019-ha-fts-220115
+export containername=mssql-mls-220312-mssql-base-2019-ha-fts-220115
+export stcgoal="SQL Server Machine Learning Services (Python and R) on Docker with an existing base"
