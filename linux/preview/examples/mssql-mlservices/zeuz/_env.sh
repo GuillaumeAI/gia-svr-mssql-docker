1d10d7e3bb829bba7597c64a708f399f25d96459
JGWill Gaia.WSL <jgi@jgwill.com>
2022-06-15 08:37:31 -0400

    add:Zeuz MSSQL ML Service on Ubuntu 16.04.
    * SQL Works
    * ML, dont know

---
+++ linux/preview/examples/mssql-mlservices/zeuz/_env.sh
@@ -0,0 +1,5 @@
+export dockertag="guillaumeai/server:zeuz-mssql-ml-ubuntu-16.04"
+export containername=zeuz-mssql-mls-220615
+export containername=zeuz
+#zaml
+export stcgoal="SSIS, SQL and SQL Server Machine Learning Services (Python and R) on Docker"
